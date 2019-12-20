c3a30d5539f5a963504f940128e117855c984c27
badogan <basri.dogan.71@gmail.com>
2019-12-20 10:50:06 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,17 @@
 class SessionsController < ApplicationController
 
     def new
-
     end
 
     def create
-        if !session[:username]
-        session[:username] = params[:name]
+        return redirect_to '/login' if !params[:name] || params[:name].empty?
+        session[:name] = params[:name]
         redirect_to '/'
     end
 
     def destroy
-        #TODO
+        session.delete :name
+        redirect_to '/'
     end
 
 end
